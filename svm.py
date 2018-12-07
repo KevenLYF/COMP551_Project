ea20bb02a634e7ded6215f13756446b92e3a1765
KevenLiu <kevenliu94@gmail.com>
2018-12-07 17:30:48 -0500

    designed new preprocessing methods

--- svm.py
+++ svm.py
@@ -1,18 +1,26 @@
 from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
+import numpy as np
+import os
+import re
+import pandas as pd
 import warnings; warnings.simplefilter('ignore')
-from utility import preprocessing
-
-trainX, trainY = preprocessing("./aclImdb/train/labeledBow.feat")
-
-c_range = []
-d_range = [True, False]
-c = 0.02
-for i in range(5):
-    c *= 0.6
-    c_range.append(c)
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from utility import preprocessing2, clean_text, load_train_test_imdb_data
+import matplotlib.pyplot as plt
 
 def gridSearch_SVM(X_train, y_train):
+
+    trainX, trainY = preprocessing2("./aclImdb/train/labeledBow.feat")
+
+    c_range = []
+    d_range = [True, False]
+    c = 0.1
+    for i in range(5):
+        c *= 0.6
+        c_range.append(c)
 
     param_grid = dict(C=c_range, dual=d_range)
     svm = LinearSVC(C=c_range, dual=d_range)
@@ -20,6 +28,46 @@ def gridSearch_SVM(X_train, y_train):
     gs.fit(X_train, y_train)
     best_score = gs.best_score_
     best_param = gs.best_params_
-    print("The F1 measure = {} \nC = {}\nDual = {}".format(best_score, best_param.get('C'), best_param.get('dual')))
+    print("The Accuracy = {} \nC = {}\nDual = {}".format(best_score, best_param.get('C'), best_param.get('dual')))
 
-gridSearch_SVM(trainX, trainY)
+# gridSearch_SVM(trainX, trainY)
+
+def SVM_ngrams(train, test, n):
+    # Transform each text into a vector of word counts
+    vectorizer = TfidfVectorizer(stop_words="english", preprocessor=clean_text, ngram_range=(1, n))
+
+    training_features = vectorizer.fit_transform(train["text"])
+    test_features = vectorizer.transform(test["text"])
+
+    temp_c = 10
+    best_acc = 0
+    max_c = 1
+    c_range = []
+    acc_range = []
+    for i in range(30):
+        model = LinearSVC(C=temp_c)
+        model.fit(training_features, train["sentiment"])
+        y_pred = model.predict(test_features)
+        acc = accuracy_score(test["sentiment"], y_pred)
+        if (acc > best_acc):
+            best_acc = acc
+            max_c = temp_c
+        c_range.append(temp_c)
+        acc_range.append(acc)
+        temp_c *= 0.7
+        print("Accuracy on the IMDB dataset: {:.2f}".format(acc * 100))
+
+    # Evaluation
+    print("Best accuracy is {:.2f} \n Best C is {}".format(best_acc * 100, max_c))
+    return c_range, acc_range
+
+def funny_plot(x, y):
+    plt.plot(x, y, 'ro')
+    plt.title('Unigram C vs Accuracy')
+    plt.xlabel('Hyper Parameter C')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+train_data, test_data = load_train_test_imdb_data(data_dir="aclImdb/")
+c_array, acc_array = SVM_ngrams(train_data, test_data, 3)
+funny_plot(c_array, acc_array)
--- utility.py
+++ utility.py
@@ -1,4 +1,7 @@
 import numpy as np
+import os
+import re
+import pandas as pd
 
 
 def preprocessing(features):
@@ -49,8 +52,6 @@ def preprocessing2(features):
 
 def readValue(file):
     with open(file, 'r') as f:
-        # for row in f:
-        #     values.append(int(row))
         values = f.read().splitlines()
 
     return values
@@ -147,7 +148,68 @@ def fvpair_binary(file):
                 f.write(" " + str(data[i][j][0]) + ":" + str(data[i][j][1]))
             f.write("\n")
 
-preprocessing2("./aclImdb/train/labeledBow.feat")[1]
+"""
+Applies some pre-processing on the given text.
+
+Steps :
+- Removing HTML tags
+- Removing punctuation
+- Lowering text
+"""
+def clean_text(text):
+    # remove HTML tags
+    text = re.sub(r'<.*?>', '', text)
+
+    # remove the characters [\], ['] and ["]
+    text = re.sub(r"\\", "", text)
+    text = re.sub(r"\'", "", text)
+    text = re.sub(r"\"", "", text)
+
+    # convert text to lowercase
+    text = text.strip().lower()
+
+    # replace punctuation characters with spaces
+    filters = '!"\'#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
+    translate_dict = dict((c, " ") for c in filters)
+    translate_map = str.maketrans(translate_dict)
+    text = text.translate(translate_map)
+
+    return text
+
+"""Loads the IMDB train/test datasets from a folder path.
+Input:
+data_dir: path to the "aclImdb" folder.
+
+Returns:
+train/test datasets as pandas dataframes.
+"""
+def load_train_test_imdb_data(data_dir):
+
+    data = {}
+    for split in ["train", "test"]:
+        data[split] = []
+        for sentiment in ["neg", "pos"]:
+            score = 1 if sentiment == "pos" else 0
+
+            path = os.path.join(data_dir, split, sentiment)
+            file_names = os.listdir(path)
+            for f_name in file_names:
+                with open(os.path.join(path, f_name), "r") as f:
+                    review = f.read()
+                    data[split].append([review, score])
+
+    np.random.shuffle(data["train"])
+    data["train"] = pd.DataFrame(data["train"],
+                                 columns=['text', 'sentiment'])
+
+    np.random.shuffle(data["test"])
+    data["test"] = pd.DataFrame(data["test"],
+                                columns=['text', 'sentiment'])
+
+    return data["train"], data["test"]
+
+
+# preprocessing2("./aclImdb/train/labeledBow.feat")[1]
 # fvpair("./aclImdb/train/labeledBow.feat")
 # for i in range(len(data)):
 #     print(data[i])
