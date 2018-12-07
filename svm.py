031698ad0f275695bb82fa68e00411c3c8894a09
KevenLiu <kevenliu94@gmail.com>
2018-12-07 10:48:14 -0500

    added more preprocessing methods

--- svm.py
+++ svm.py
@@ -7,16 +7,16 @@ trainX, trainY = preprocessing("./aclImdb/train/labeledBow.feat")
 
 c_range = []
 d_range = [True, False]
-c = 1
-for i in range(20):
-    c *= 0.8
+c = 0.02
+for i in range(5):
+    c *= 0.6
     c_range.append(c)
 
 def gridSearch_SVM(X_train, y_train):
 
     param_grid = dict(C=c_range, dual=d_range)
     svm = LinearSVC(C=c_range, dual=d_range)
-    gs = GridSearchCV(svm, param_grid, scoring='f1_micro', n_jobs=-1, verbose=50)
+    gs = GridSearchCV(svm, param_grid, scoring='accuracy', n_jobs=-1, verbose=50)
     gs.fit(X_train, y_train)
     best_score = gs.best_score_
     best_param = gs.best_params_
--- utility.py
+++ utility.py
@@ -46,6 +46,13 @@ def preprocessing2(features):
 
     return x, y
 
+def readValue(file):
+    with open(file, 'r') as f:
+        # for row in f:
+        #     values.append(int(row))
+        values = f.read().splitlines()
+
+    return values
 
 def fvpair(file):
     lcount = 0
@@ -77,12 +84,72 @@ def fvpair(file):
             for j in range(1, len(data[i])):
                 f.write(" " + str(data[i][j][0]) + ":" + str(data[i][j][1]))
 
+def fvpair_value(file, value):
+    lcount = 0
+    with open(file, 'r') as f:
+        data = []
+        for row in f:
+            newRow = []
+            lcount += 1
+            feature = row[1:]
+            rating = int(row[0])
+            if rating <= 4:
+                rating = -1
+            else:
+                rating = 1
+            newRow.append(rating)
+            for ratio in feature.split(' ')[1:]:
+                pair = []
+                ratio = ratio.split(':')
+                word = int(ratio[0])
+                word += 1
+                pair.append(word)
+                pair.append(value[int(ratio[0])])
+                newRow.append(pair)
+            data.append(newRow)
 
+    with open('test.dat', 'w') as f:
+        for i in range(len(data)):
+            f.write(str(data[i][0]))
+            for j in range(1, len(data[i])):
+                f.write(" " + str(data[i][j][0]) + ":" + str(data[i][j][1]))
+            f.write("\n")
 
-preprocessing("./aclImdb/train/labeledBow.feat")[0]
-#fvpair("./aclImdb/train/labeledBow.feat")
-# fvpair("./aclImdb/test/labeledBow.feat")
+def fvpair_binary(file):
+    lcount = 0
+    with open(file, 'r') as f:
+        data = []
+        for row in f:
+            newRow = []
+            lcount += 1
+            feature = row[1:]
+            rating = int(row[0])
+            if rating <= 4:
+                rating = -1
+            else:
+                rating = 1
+            newRow.append(rating)
+            for ratio in feature.split(' ')[1:]:
+                pair = []
+                ratio = ratio.split(':')
+                word = int(ratio[0])
+                word += 1
+                pair.append(word)
+                pair.append(1)
+                newRow.append(pair)
+            data.append(newRow)
+
+    with open('test.dat', 'w') as f:
+        for i in range(len(data)):
+            f.write(str(data[i][0]))
+            for j in range(1, len(data[i])):
+                f.write(" " + str(data[i][j][0]) + ":" + str(data[i][j][1]))
+            f.write("\n")
+
+# preprocessing("./aclImdb/train/labeledBow.feat")[0]
+# fvpair("./aclImdb/train/labeledBow.feat")
 # for i in range(len(data)):
 #     print(data[i])
 
+# fvpair_binary("./aclImdb/test/labeledBow.feat")
 
